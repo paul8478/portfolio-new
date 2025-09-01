388f5ff07b3d196e15b65d10c285d4ba9cc99d35
paul8478 <somnathpaul626@gmail.com>
2025-09-01 16:51:07 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsPage";
+import Footer from "./components/Footer"; // ✅ fixed spelling
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <div style={{ padding: "20px" }}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-        </Routes>
+      <div className="flex flex-col min-h-screen">
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Page Content */}
+        <main className="flex-grow p-5">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+          </Routes>
+        </main>
+
+        {/* Footer */}
+        <Footer />
       </div>
     </Router>
   );
